7a55b814a07c1b47f8ea411f287e6e95ac30f3a5
Karl Groff <twistinside@users.noreply.github.com>
2024-01-11 21:24:36 -0800

    Add down time

--- sh/reboot.sh
+++ sh/reboot.sh
@@ -9,8 +9,20 @@ api_url="$MASTODON_URL/api/v1/statuses"
 # Access token (from config file)
 access_token="$ACCESS_TOKEN"
 
+# Get the current time as the reboot time in seconds since epoch
+reboot_sec=$(date '+%s')
+
+# Get the last shutdown time
+last_shutdown=$(last -x | grep shutdown | head -1 | awk '{ print $5 " " $6 " " $7 " " $8 }')
+
+# Convert times to seconds since epoch
+shutdown_sec=$(date -d "$last_shutdown" '+%s')
+
+# Calculate downtime in seconds
+downtime=$((reboot_sec - shutdown_sec))
+
 # Message to post
-message="I just woke up from a reboot."
+message="I just woke up from a reboot.%0AI was asleep for $downtime seconds... What did I miss?"
 
 # Log file location within the private Mastodon folder
 log_file="$HOME/.mastodon/api.log"
